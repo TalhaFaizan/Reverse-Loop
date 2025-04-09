f1c21c05af8ac253d83aa7e4f2e0fcf25b5f8bb3
Talha Faizan <140506077+TalhaFaizan@users.noreply.github.com>
2025-04-08 20:11:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+console.log("REVERSE LOOP");
 for (let i = 10; i>=1;i--){
        console.log(i);
     
+}
+console.log("REVERSE LOOP WHILE SKIPPING 6");
+for (let i = 10; i >= 1; i--) {
+  if (i != 6) console.log(i);
 }
